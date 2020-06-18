bce04f7fb35d8751686e9d0801a18004d3b98ae2
Gaoyagi <aoyagigeorge@gmail.com>
2020-06-17 21:01:03 -0700

    finished page 1

---
+++ server.js
@@ -0,0 +1,8 @@
+// Require Libraries
+const express = require('express');
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
